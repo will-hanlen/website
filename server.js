d6ca7d4108cc74686a333099d4549fed0a1627bb
Will Hanlen <17443406+will-hanlen@users.noreply.github.com>
2020-02-26 12:52:24 -0700

    remove favicon

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 // Imports
 const express = require('express')
 const path = require('path')
-const favicon = require('serve-favicon')
 
 // Init app
 const app = express()
-
-// Serve favicon
-app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.png')))
 
 // Display static site
 app.use(express.static('public'))
