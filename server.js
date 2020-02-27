02ae51433a7b0d09a5d0dc67c8e29243224478fe
Will Hanlen <17443406+will-hanlen@users.noreply.github.com>
2020-02-26 19:29:33 -0700

    re-add favicon

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Imports
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 // Init app
 const app = express()
+
+// Serve favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 
 // Display static site
 app.use(express.static('public'))
