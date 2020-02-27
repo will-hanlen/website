d163f37ce21a4dd856b5cd77d88b4f1f4608e53e
Will Hanlen <17443406+will-hanlen@users.noreply.github.com>
2020-02-26 18:43:38 -0700

    route server for simpler URLs

--- server.js
+++ server.js
@@ -8,5 +8,17 @@ const app = express()
 // Display static site
 app.use(express.static('public'))
 
+// Short url routing
+app.get(/^\/([0-9a-zA-Z\-\_]+)$/, function(req, res) {
+  // res.send(req.params)
+  res.sendFile(`${req.params[0]}.html`, {root: path.join(__dirname, "public", "writing")})
+})
+
+// Image url simplification
+app.get(/^\/([0-9a-zA-Z\-\_]+)\.svg$/, function(req, res) {
+  // res.send(req.params)
+  res.sendFile(`${req.params[0]}.svg`, {root: path.join(__dirname, "public", "writing")})
+})
+
 // Start server
 app.listen(8080)
